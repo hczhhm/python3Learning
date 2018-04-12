65a535e976280b9e5dde2f28256919cf3faa8c7a
hczhhm <huangczh109@126.com>
2018-04-12 16:21:32 +0800

    Revert "Revert "Initial commit""
    This reverts commit 28441feb20db3363adcb301bd654b82759373734.

---
+++ 基础学习.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+#黄致
+
+x="a"
+y="b"
+# 换行输出
+print( x )
+print( y )
+
+print('---------')
+# 不换行输出
+print( x, end=" " )
+print( y, end=" " )
+print()
+
+counter = 100
+miles = 1000.0
+name = 'runoob'
+
+print(counter)
+print(miles)
+print(name)
+
+a = b = c = 12
+print(a,b,c,)
+a,b,c = 1,3,'nihao'
+print(a,b,c,)
