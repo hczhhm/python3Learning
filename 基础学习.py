5bb5b402c54a9b38b1c3134b86608f40f64b814d
hczhhm <huangczh109@126.com>
2018-04-12 17:52:47 +0800

    Revert "Revert "Merge branch 'master' of https://github.com/hczhhm/python3Learning""
    This reverts commit 788edee3a83591c274c26aee7559c9159dbee941.

--- 基础学习.py
+++ 基础学习.py
@@ -26,3 +26,32 @@ a = b = c = 12
 print(a,b,c,)
 a,b,c = 1,3,'nihao'
 print(a,b,c,)
+
+print('\n类型：\n')
+a,b,c,d = 20,5.5,True,4+3j
+print(type(a),type(b),type(c),type(d))
+print(isinstance(b,complex))
+
+print('\n数值运算：\n')
+print(5+4)
+print(4.3-2)
+print(3*7)
+print(2/4)
+print(2//4)
+print(17%3)
+print(2**5)
+
+print('\n字符串：\n')
+str = 'Runoob'
+print(str)
+print(str[0:-1])
+print(str[0])
+print(str[2:5])
+print(str[2:])
+print(str*2)
+print(str + 'TEST')
+
+
+word = 'python'
+print(word[0],word[5])
+print(word[-1],word[-6])
